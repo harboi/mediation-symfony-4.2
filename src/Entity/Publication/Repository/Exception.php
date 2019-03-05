6b11c4eed830d8aeaf570843aa1ca43e9668fbe6
Harold BOISSENIN <boissenin@gmail.com>
2019-03-05 23:56:59 +0100

    add exceptions for Publication entity

---
+++ src/Entity/Publication/Repository/Exception.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harmedia
+ * Date: 05/03/2019
+ * Time: 20:00
+ */
+
+namespace App\Entity\Publication\Repository;
+
+class Exception extends \Exception
+{
+}
---
+++ src/Entity/Publication/Repository/Exception/PublicationNotFound.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harmedia
+ * Date: 05/03/2019
+ * Time: 20:00
+ */
+
+namespace App\Entity\Publication\Repository\Exception;
+
+use App\Entity\Publication\Repository\Exception;
+
+class PublicationNotFound extends Exception
+{
+}
