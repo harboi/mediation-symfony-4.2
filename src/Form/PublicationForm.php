705e281773c2d66aee06116e2a9003c3369ee7ec
Harold BOISSENIN <boissenin@gmail.com>
2019-03-10 22:56:06 +0100

    update publication form + template

--- src/Form/PublicationForm.php
+++ src/Form/PublicationForm.php
@@ -10,6 +10,10 @@ namespace App\Form;
 
 use App\Entity\Publication;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,20 +23,19 @@ class PublicationForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('publishedAt', 'date', array(
+        $builder->add('publishedAt', DateType::class, array(
             'input'  => 'datetime',
             'widget' => 'choice',
             'label' => 'Date publication',
         ));
 
-        $builder->add('titre', 'text',
+        $builder->add('title', TextType::class,
             array('label' => 'Titre', 'attr' => [], 'constraints' => array(new Assert\NotBlank(), new Assert\Length(array('max'  => 200)))));
 
-        $builder->add('texte', 'textarea',
-            array('label' => 'Texte', 'attr' => ['style' => 'height:250px;'], 'constraints' => array(new Assert\NotBlank())));
+        $builder->add('content', TextareaType::class,
+            array('label' => 'Texte', 'attr' => ['style' => 'height:400px;'], 'constraints' => array(new Assert\NotBlank())));
 
-
-        $builder->add('submit', 'submit',
+        $builder->add('submit', SubmitType::class,
             array('label' => 'Envoyer', 'attr' => array('class' => 'btn-primary btn-lg')));
     }
 
